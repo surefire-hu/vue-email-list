90234b9b3fd8e68fe80e0236a26c279e646966e0
surefire-hu <surefire.hu2001@gmail.com>
2024-09-02 19:32:42 +0200

    creato ciclo per le mail e la stampa in html quando sono 10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+const { createApp } = Vue;
 
+const app = createApp({
+    data() {
+        return {
+            message: 'Email List',
+            emails: []
+        };
+    },
+    // ciclo per chiamare l'API 10 volte
+    created() { 
+        const emailPromises = [];
+        for (let i = 0; i < 10; i++) {
+            emailPromises.push(
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => resp.data.response)
+            )    
+        }   
+        // ciclo per creare le email
+        Promise.all(emailPromises)
+        .then((emails) => {
+            this.emails = emails;
+        })
+    }
+}).mount('#app');
